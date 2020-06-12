24c7fba6d3a1ec01fc6fdacd965c0368b2cbe69d
Keenan <keenanhallas@gmail.com>
2020-06-12 17:27:26 -0500

    tests working for checking existence of inputs

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,43 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, getByText, fireEvent, waitFor, screen } from "@testing-library/react";
 import App from "./App";
 
 test("renders App without crashing", () => {
   render(<App />);
 });
+
+test("a sanity test", () => {
+  expect(true).toBe(true);
+});
+
+describe("checks for all form inputs", () => {
+  test("first name component is visible", () => {
+    const {getByTestId} = render(<App />);
+    const firstName = getByTestId(/firstName/i);
+    expect(firstName).toBeInTheDocument();
+  })
+
+  test("last name component is visible", () => {
+    const {getByTestId} = render(<App />);
+    const lastName = getByTestId(/lastName/i);
+    expect(lastName).toBeInTheDocument();
+  })
+
+  test("email component is visible", () => {
+    const {getByTestId} = render(<App />);
+    const email = getByTestId(/email/i);
+    expect(email).toBeInTheDocument();
+  })
+
+  test("message component is visible", () => {
+    const {getByTestId} = render(<App />);
+    const message = getByTestId(/message/i);
+    expect(message).toBeInTheDocument();
+  })
+})
+
+test("error message for first name field should only show up for less than 3 characters", () => {
+  const {getByTestId, change} = render(<App />);
+  const firstName = getByTestId(/firstName/i);
+  fireEvent.change(firstName, { target: { value: 'Keenan' } });
+})
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -17,6 +17,7 @@ const ContactForm = () => {
           <label htmlFor="firstName">First Name*</label>
           <input
             name="firstName"
+            data-testid="firstName"
             placeholder="Edd"
             ref={register({ required: true, maxLength: 3 })}
           />
@@ -29,6 +30,7 @@ const ContactForm = () => {
           <label htmlFor="lastName">Last Name*</label>
           <input
             name="lastName"
+            data-testid="lastName"
             placeholder="Burke"
             ref={register({ required: true })}
           />
@@ -41,14 +43,14 @@ const ContactForm = () => {
           <label htmlFor="email" placeholder="bluebill1049@hotmail.com">
             Email*
           </label>
-          <input name="email" ref={register({ required: true })} />
+          <input name="email" data-testid="email" ref={register({ required: true })} />
           {errors.email && (
             <p>Looks like there was an error: {errors.email.type}</p>
           )}
         </div>
         <div>
           <label htmlFor="message">Message</label>
-          <textarea name="message" ref={register({ required: false })} />
+          <textarea name="message" data-testid="message" ref={register({ required: false })} />
         </div>
         {data && (
           <pre style={{ textAlign: "left", color: "white" }}>
